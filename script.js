dac8fd1696bab3c97ccb22002c95d32c33b6748a
Rachel Denucci <racheldenucci@yahoo.com.br>
2022-07-13 10:58:20 -0300

    checkNum function

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const mais = document.getElementById('mais');
 const menos = document.getElementById('menos');
 const reset = document.getElementById('reset');
-const change = document.getElementById('changeColor');
-let container = document.getElementById('container');
 
+let container = document.getElementById('container');
 let int = document.getElementById('num');
 let cont = 0;
 
 mais.addEventListener('click', function(){
     cont++;
     int.innerHTML = cont;
+    checkNum(cont)
 })
 
 menos.addEventListener('click', function(){
     cont--;
     int.innerHTML = cont;
+    checkNum(cont)
 })
 
 reset.addEventListener('click', function(){
     cont = 0;
     int.innerHTML = cont;
-    container.style.backgroundColor = "cornflowerblue";
+    container.style.backgroundColor = "gray";
 })
 
-change.addEventListener('click', function(){
-    container.style.backgroundColor = "red"; 
-})
+function checkNum(cont){
+    if(cont < 0){
+        cont = 0;
+        int.innerHTML = cont;
+        alert('Limite atingido');
+    }
 
+    if(cont > 10){
+        cont = 10;
+        int.innerHTML = cont;
+        alert('Limite atingido');
+    }
+
+    if(cont % 2 != 0){
+        container.style.backgroundColor = "teal";
+    }
+    else if(cont == 0){
+        container.style.backgroundColor = "gray";
+    }
+    else{
+        container.style.backgroundColor = "maroon";
+    }
+}
